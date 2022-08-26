a58290ca43dfa23234333bc3bcbb4b7b32cb035a
sayeedkhannabil <sayeedkhannabil@gmail.com>
2022-08-25 19:49:36 -0500

    salatTiming call changed to https to run on github

--- assets/js/salatTimings.js
+++ assets/js/salatTimings.js
@@ -2,7 +2,7 @@
 
 async function getSalatTime() {
     try {
-        const response = await axios.get('http://api.aladhan.com/v1/timingsByCity?city=Winnipeg&country=Canada&method=2');
+        const response = await axios.get('https://api.aladhan.com/v1/timingsByCity?city=Winnipeg&country=Canada&method=2');
         return response;
     } catch (error) {
         console.error(error);
